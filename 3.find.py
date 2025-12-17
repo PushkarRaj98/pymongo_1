ea5da2fabc3f2d909494560658bfb97356798c10
PushkarRaj98 <rajpushkar7770@gmail.com>
2025-12-17 11:36:40 +0530

    use of operator

--- 3.find.py
+++ 3.find.py
@@ -4,17 +4,17 @@ if __name__=="__main__":
     print("welcome to pymongo ")
     client = p.MongoClient("mongodb://localhost:27017/")
     print("connected.......")
-    db=client['harry'] 
-    collection= db['sampleclollection']
+    db=client["harry"] 
+    collection= db["sampleclollection"]
     
 
 
-    # one =collection.find_one ({'name':'sam'})
+    # one =collection.find_one ({"name":"sam"})
     # print(one)
 
     #there is no find many() so we have to find it manualy using loop 
-    alldocs= collection.find({'name':'samar'},{'name': 1,'marks':1,'_id':0})
+    alldocs= collection.find({"marks":{"$lte":70}},{"name": 1,"_id":0})
     for i in alldocs:
         print(i)         
         
-
+ 
